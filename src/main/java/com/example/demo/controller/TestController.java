8b72a4ca797722c8e9eea193b9a7ae9946d43ef6
hqintian <510141417@qq.com>
2021-10-01 13:22:26 +0800

    测试提交

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -18,7 +18,7 @@ public class TestController {
         Entry entry = null;
         try {
             entry = SphU.entry("hello1");
-            // 资源中的逻辑.
+            // 资源中的逻辑. 啊啊啊
             System.out.println(Thread.currentThread().getId() + " Hello Sentinel1");
         } catch (BlockException e1) {
             System.out.println("blocked!");
